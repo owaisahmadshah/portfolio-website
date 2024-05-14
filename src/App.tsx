154cc4d3c57620f6d55b5a052cb4ff06a4803568
Owais <owaisahmadqureshi019@gmail.com>
2024-05-14 15:11:59 +0500

    Added projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Home from "@/components/Home"
 import Navbar from "@/components/Navbar"
+import Projects from "@/components/Projects"
 
 function App() {
   return (
     <>
     <Navbar />
     <Home />
+    <Projects />
     </>
   )
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+    return (
+        <section id="Home" className="flex h-[60vh] bg-gray-500 min-h-[100vh]">
+            <p className="font-bold mx-auto my-auto">
+                Projects comming soon...
+            </p>
+        </section>
+    )
+}
+
+export default Projects
