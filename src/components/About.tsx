e0e3bb1c78fca9d1d5fa435dd4d218d1222f6dcc
Owais <owaisahmadqureshi019@gmail.com>
2024-05-21 11:33:46 +0500

    Added just about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,11 @@
+import { Separator } from "@/components/ui/separator"
+
 const About = () => {
     return (
         <section id="About" className="flex h-[60vh] bg-gray-500 min-h-[100vh]">
-            <p className="font-bold mx-auto my-auto">
-                About comming soon...
-            </p>
+            <h1 className="mx-auto text-white text-4xl font-bold uppercase my-12">About Me</h1>
+            <Separator />
+
         </section>
     )
 }
