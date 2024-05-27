934421dfb21d5d5b5d680d1b6811012b1e2bc831
Owais <owaisahmadqureshi019@gmail.com>
2024-05-27 14:27:38 +0500

    Changed Scroll Area.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import Projects from "@/components/Projects"
 import About from "@/components/About"
 import Contact from "@/components/Contact"
 import Footer from "./components/Footer"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 function App() {
   return (
-    <>
+    <ScrollArea className="h-[100vh] rounded-md border">
       <Navbar />
       <Home />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+    </ScrollArea>
   )
 }
 
