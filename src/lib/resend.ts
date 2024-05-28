a88ebad6bdfe9030864d683826ed0ce47d385a5a
Owais <owaisahmadqureshi019@gmail.com>
2024-05-28 14:21:33 +0500

    FIX resend.ts

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,3 +1,4 @@
 import { Resend } from 'resend'
 
-export const resend = new Resend(process.env.REACT_APP_RESEND_API_KEY)
+// export const resend = new Resend(import.meta.env.REACT_APP_RESEND_API_KEY)
+export const resend = new Resend("re_QG97iWSE_97rEKNbqsJP56hQAMaw2T9YW")
