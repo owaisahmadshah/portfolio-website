4e2a7832f259a899019b14195a783d523c531617
Owais <owaisahmadqureshi019@gmail.com>
2024-05-28 12:54:48 +0500

    Added responsiveness to  navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ interface NavbarProps { }
 
 const Navbar: React.FC<NavbarProps> = () => {
     return (
-        <nav className='fixed w-[100vw] h-[12vh] top-0 z-10 px-10 py-5 flex items-center justify-between bg-white'>
+        <nav className='fixed w-[100vw] h-[12vh] top-0 z-10 px-10 py-5 flex md:flex-row items-center justify-center gap-2 md:justify-between bg-white'>
             <div className='flex items-center gap-5'>
                 <a href="#home">
                     <Avatar>
@@ -12,9 +12,9 @@ const Navbar: React.FC<NavbarProps> = () => {
                         <AvatarFallback>Owais</AvatarFallback>
                     </Avatar>
                 </a>
-                <a href="#home" className='text-xl font-bold'>Owais Ahmad Shah</a>
+                <a href="#home" className='text-xl font-bold hidden md:block'>Owais Ahmad Shah</a>
             </div>
-            <ul className='flex justify-center items-center gap-14 text-base font-semibold'>
+            <ul className='flex justify-center items-center gap-4 md:gap-14 text-base font-semibold'>
                 <li>
                     <a href="#home" className='hover-nav-items'>
                         HOME
