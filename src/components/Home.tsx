3381be9dda62ed33440baf2c75a127cef0fdeed1
Owais <owaisahmadqureshi019@gmail.com>
2024-05-28 13:02:49 +0500

    Added responsiveness to the homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
     return (
-        <section id="Home" className="flex h-[60vh] bg-blue-500 min-h-[100vh] text-white">
-            <div className="w-[15vw] flex flex-col justify-center items-center gap-6">
+        <section id="Home" className="flex flex-col-reverse justify-center gap-14 md:gap-0 items-center md:flex-row bg-blue-500 min-h-[100vh] text-white">
+            <div className="w-[15vw] flex md:flex-col justify-center items-center gap-6">
                 <div className="social-media-icon">
                     <LinkedinIcon className="h-10 w-10 cursor-pointer" />
                 </div>
