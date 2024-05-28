913ff0324004c9dd32416fa08d88f4912962fadf
Owais <owaisahmadqureshi019@gmail.com>
2024-05-28 13:14:18 +0500

    Added responsiveness to the project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,34 +8,34 @@ const Projects = () => {
             <Separator className="max-w-8 h-2 mx-auto rounded-sm bg-gray-950 my-6" />
             <p className="w-[80vw] mx-auto text-center mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur iste veritatis maxime officiis enim sapiente beatae exercitationem ipsum delectus fugiat ullam, adipisci accusamus labore impedit totam perferendis at quidem voluptatum?</p>
 
-            <div className="flex justify-around">
-                <div className="w-[60vw] flex justify-center items-center">
+            <div className="flex justify-around flex-col md:flex-row">
+                <div className="md:w-[60vw] flex justify-center items-center">
                     {/* <img src="../../public/projectImage.jpeg" alt="Projec1" /> */}
                     <img src="" alt="Projec1" />
                 </div>
-                <div className="w-[40vw]">
+                <div className="md:w-[40vw] mx-6 md:mx-0">
                     <h1 className="text-3xl font-bold mt-12">Project 1</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis iure, aperiam earum, facilis numquam nesciunt excepturi velit laborum omnis libero impedit neque consequatur sed odio ex minima ut quibusdam? Quis, neque! Cumque inventore eos velit vero quo praesentium suscipit.</p>
                     <Button className="my-6 bg-blue-500">More about Project</Button>
                 </div>
             </div>
-            <div className="flex justify-around">
-                <div className="w-[60vw] flex justify-center items-center">
+            <div className="flex justify-around flex-col md:flex-row">
+                <div className="md:w-[60vw] flex justify-center items-center">
                     {/* <img src="../../public/projectImage.jpeg" alt="Project2" /> */}
                     <img src="" alt="Project2" />
                 </div>
-                <div className="w-[40vw]">
+                <div className="md:w-[40vw] mx-6 md:mx-0">
                     <h1 className="text-3xl font-bold mt-12">Project 2</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis iure, aperiam earum, facilis numquam nesciunt excepturi velit laborum omnis libero impedit neque consequatur sed odio ex minima ut quibusdam? Quis, neque! Cumque inventore eos velit vero quo praesentium suscipit.</p>
                     <Button className="my-6 bg-blue-500">More about Project</Button>
                 </div>
             </div>
-            <div className="flex justify-around">
-                <div className="w-[60vw] flex justify-center items-center">
+            <div className="flex justify-around flex-col md:flex-row">
+                <div className="md:w-[60vw] flex justify-center items-center">
                     {/* <img src="../../public/projectImage.jpeg" alt="Project3" /> */}
                     <img src="" alt="Project3" />
                 </div>
-                <div className="w-[40vw]" >
+                <div className="md:w-[40vw] mx-6 md:mx-0" >
                     <h1 className="text-3xl font-bold mt-12">Project 3</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis iure, aperiam earum, facilis numquam nesciunt excepturi velit laborum omnis libero impedit neque consequatur sed odio ex minima ut quibusdam? Quis, neque! Cumque inventore eos velit vero quo praesentium suscipit.</p>
                     <Button className="my-6 bg-blue-500">More about Project</Button>
