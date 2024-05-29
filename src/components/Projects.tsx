47fccd047cb489710ad1650cc03ab9be46c93d7a
Owais <owaisahmadqureshi019@gmail.com>
2024-05-29 17:50:23 +0500

    Added project descriptions

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
         <section id="Projects" className="flex flex-col bg-gray-100 minh-[100vh]">
             <h1 className="mx-auto text-4xl font-bold uppercase mt-12">Projects</h1>
             <Separator className="max-w-8 h-2 mx-auto rounded-sm bg-gray-950 my-6" />
-            <p className="w-[80vw] mx-auto text-center mb-6">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur iste veritatis maxime officiis enim sapiente beatae exercitationem ipsum delectus fugiat ullam, adipisci accusamus labore impedit totam perferendis at quidem voluptatum?</p>
+            <p className="w-[80vw] mx-auto text-center mb-6">Welcome to my project portfolio! Here you'll find a showcase of my work, highlighting various projects that demonstrate my skills and experience in web development. Each project is a reflection of my commitment to building efficient, user-friendly, and innovative web applications. Feel free to explore and learn more about the projects I've worked on and the technologies I've utilized.</p>
 
             <div className="flex justify-around flex-col md:flex-row">
                 <div className="md:w-[60vw] flex justify-center items-center">
@@ -26,17 +26,6 @@ const Projects = () => {
                 </div>
                 <div className="md:w-[40vw] mx-6 md:mx-0">
                     <h1 className="text-3xl font-bold mt-12">Project 2</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis iure, aperiam earum, facilis numquam nesciunt excepturi velit laborum omnis libero impedit neque consequatur sed odio ex minima ut quibusdam? Quis, neque! Cumque inventore eos velit vero quo praesentium suscipit.</p>
-                    <Button className="my-6 bg-blue-500 hover:bg-blue-600">More about Project</Button>
-                </div>
-            </div>
-            <div className="flex justify-around flex-col md:flex-row">
-                <div className="md:w-[60vw] flex justify-center items-center">
-                    {/* <img src="../../public/projectImage.jpeg" alt="Project3" /> */}
-                    <img src="" alt="Project3" />
-                </div>
-                <div className="md:w-[40vw] mx-6 md:mx-0" >
-                    <h1 className="text-3xl font-bold mt-12">Project 3</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quis iure, aperiam earum, facilis numquam nesciunt excepturi velit laborum omnis libero impedit neque consequatur sed odio ex minima ut quibusdam? Quis, neque! Cumque inventore eos velit vero quo praesentium suscipit.</p>
                     <Button className="my-6 bg-blue-500 hover:bg-blue-600">More about Project</Button>
                 </div>
