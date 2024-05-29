cf27251b1909398141f79f5b87e3be83b677faed
Owais <owaisahmadqureshi019@gmail.com>
2024-05-29 17:17:06 +0500

    Fixed .env issue
    FIX cors policy

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,3 @@
 import { Resend } from 'resend'
 
-// export const resend = new Resend(import.meta.env.REACT_APP_RESEND_API_KEY)
-export const resend = new Resend("re_QG97iWSE_97rEKNbqsJP56hQAMaw2T9YW")
+export const resend = new Resend(import.meta.env.VITE_RESEND_API_KEY)
