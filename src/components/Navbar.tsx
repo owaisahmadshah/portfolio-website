d00effadc74e493350497016298fb57b5fa8768f
Owais <owaisahmadqureshi019@gmail.com>
2024-06-01 17:38:07 +0500

    Added Navbar Image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import logo from "@/assets/portfolio.jpeg"
 
 interface NavbarProps { }
 
@@ -6,12 +7,10 @@ const Navbar: React.FC<NavbarProps> = () => {
     return (
         <nav className='fixed w-[100vw] h-[12vh] top-0 z-10 px-10 py-5 flex md:flex-row items-center justify-center gap-2 md:justify-between bg-white'>
             <div className='flex items-center gap-5'>
-                <a href="#Home">
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>Owais</AvatarFallback>
-                    </Avatar>
-                </a>
+                <Avatar>
+                    <AvatarImage src={logo ?? "https://github.com/shadcn.png"} />
+                    <AvatarFallback>Owais</AvatarFallback>
+                </Avatar>
                 <a href="#Home" className='text-xl font-bold hidden md:block'>Owais Ahmad Shah</a>
             </div>
             <ul className='flex justify-center items-center gap-4 md:gap-14 text-sm md:text-base font-semibold'>
